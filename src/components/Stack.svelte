c03e955c130797157baa9fa4e0124112671fd8a7
zachczx <zixian.chen@gmail.com>
2024-02-02 23:58:41 +0800

    Update Stack.svelte

--- src/components/Stack.svelte
+++ src/components/Stack.svelte
@@ -77,7 +77,7 @@
     <h3 class="">Courses/Certificates</h3>
     <span class="btn btn-xs btn-primary btn-outline hover:btn-accent text-base my-1">Meta Back-end Developer Specialization</span>
     <span class="btn btn-xs btn-primary btn-outline hover:btn-accent text-base my-1">AWS Fundamentals Specialization</span>
-    <a on:click={() => {googleopen = !googleopen}} href="#googleopen">
+    <a on:click={() => {googleopen = !googleopen}} href="#">
         <span class="btn btn-xs btn-primary btn-outline hover:btn-accent text-base my-1">Google UX
             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrows-maximize inline" width="16" height="16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M16 4l4 0l0 4" /><path d="M14 10l6 -6" /><path d="M8 20l-4 0l0 -4" /><path d="M4 20l6 -6" /><path d="M16 20l4 0l0 -4" /><path d="M14 14l6 6" /><path d="M8 4l-4 0l0 4" /><path d="M4 4l6 6" /></svg>
         </span>
