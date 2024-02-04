13de464d9eca7379e308d3ac8831839f248a2de8
zachczx <zixian.chen@gmail.com>
2024-02-05 02:36:12 +0800

    Update Landing.svelte

--- src/components/Landing.svelte
+++ src/components/Landing.svelte
@@ -13,14 +13,14 @@
     </form>
   </dialog>
 
-<div class="grid grid-cols-1 lg:grid-cols-12 items-center w-full px-7 h-full">
-    <div class="lg:col-span-5 avatar justify-self-center w-96 h-96">
+<div class="grid grid-cols-1 lg:grid-cols-12 items-center w-full h-full">
+    <div class="lg:col-span-3 justify-end"><p class="text-5xl text-center lg:text-right">Hello, I'm <b>Zixian</b>.</p></div>
+    <div class="lg:col-span-3 avatar justify-self-center w-96 h-96">
         <div class="rounded-full">
             <a href=""><img src={profilepic} class="object-scale-down object-center" alt="AI generated, so it's me but not rly." on:click={() => {my_modal_2.showModal()}}></a>
         </div>
     </div>
-    <div class="lg:col-span-7">
-        <p class="mb-12 text-5xl">Hello, I'm <b>Zixian</b>.</p>
+    <div class="lg:col-span-6">
         <p class="text-2xl">By day, I'm a <b>public servant</b>.</p>
         <p class="text-2xl mb-6">I do <b>digitalization</b> and <b>digital products</b>.</p>
         <a href="#experience"><button class="btn btn-success text-neutral text-xl me-3">
@@ -34,9 +34,9 @@
                 LinkedIn
             </button>
         </a>
-        <div class="divider divider-base-200 w-3/6 my-8"></div>
+        <div class="divider divider-base-200 w-6/6 my-8"></div>
         <p class="text-2xl font-mono">By night, I'm a <b>webdev</b> wannabe.</p>
-        <p class="text-2xl mb-6 font-mono">I enjoy making <b>web apps</b>.</p>
+        <p class="text-2xl mb-6 font-mono">I enjoy trying to make <b>web apps</b>.</p>
         <a href="#stack"><button class="btn btn-accent text-neutral text-xl hover:btn-accent font-mono me-3">
             <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-world-www" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M19.5 7a9 9 0 0 0 -7.5 -4a8.991 8.991 0 0 0 -7.484 4" /><path d="M11.5 3a16.989 16.989 0 0 0 -1.826 4" /><path d="M12.5 3a16.989 16.989 0 0 1 1.828 4" /><path d="M19.5 17a9 9 0 0 1 -7.5 4a8.991 8.991 0 0 1 -7.484 -4" /><path d="M11.5 21a16.989 16.989 0 0 1 -1.826 -4" /><path d="M12.5 21a16.989 16.989 0 0 0 1.828 -4" /><path d="M2 10l1 4l1.5 -4l1.5 4l1 -4" /><path d="M17 10l1 4l1.5 -4l1.5 4l1 -4" /><path d="M9.5 10l1 4l1.5 -4l1.5 4l1 -4" /></svg>
             Webdev-wannabe
