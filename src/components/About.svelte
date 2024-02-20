5081e94d2c4aec272fa20b12c7f812d5a4d49727
zachczx <zixian.chen@gmail.com>
2024-02-20 17:04:52 +0800

    added images of experience and changed landing page

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -12,7 +12,7 @@ let dxotoggle = false
     <div>
         <div class="card bg-accent-content shadow-xl">
             <div class="card-body">
-                <h2 class="card-title"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-pin" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /><path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" /></svg> 
+                <h2 class="card-title text-2xl xl:text-3xl"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-map-pin" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /><path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" /></svg> 
                     Occifer
                 </h2>
               <p>I'm a civil serpent.</p>
@@ -32,7 +32,7 @@ let dxotoggle = false
         <div class="card bg-accent-content shadow-xl">
             <figure><img src={corgiConstruction} alt="Corgi at work" class="object-scale-down" loading="lazy" /></figure>
             <div class="card-body">
-                <h2 class="card-title">
+                <h2 class="card-title text-2xl xl:text-3xl">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shovel" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M17 4l3 3" /><path d="M18.5 5.5l-8 8" /><path d="M8.276 11.284l4.44 4.44a.968 .968 0 0 1 0 1.369l-2.704 2.704a4.108 4.108 0 0 1 -5.809 -5.81l2.704 -2.703a.968 .968 0 0 1 1.37 0z" /></svg>
                     Work work
                 </h2>
@@ -45,7 +45,7 @@ let dxotoggle = false
     <div>
         <div class="card bg-accent-content shadow-xl">
             <div class="card-body">
-                <h2 class="card-title">
+                <h2 class="card-title text-2xl xl:text-3xl">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-office" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 18h9v-12l-5 2v5l-4 2v-8l9 -4l7 2v13l-7 3z" /></svg> 
                     Office
                 </h2>
@@ -53,18 +53,16 @@ let dxotoggle = false
             </div>
             <figure><img src={corgiPresentationCard} alt="Corgi presenting" class="object-scale-down" loading="lazy" /></figure>
         </div>
-        
-        <p></p>
     </div>
     <div>
         <div class="card bg-accent-content shadow-xl">
             <figure><img src={corgiProgrammingCard} alt="Corgi programming" class="object-scale-down" loading="lazy" /></figure>
             <div class="card-body">
-                <h2 class="card-title">
+                <h2 class="card-title text-2xl xl:text-3xl">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-device-desktop" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 5a1 1 0 0 1 1 -1h16a1 1 0 0 1 1 1v10a1 1 0 0 1 -1 1h-16a1 1 0 0 1 -1 -1v-10z" /><path d="M7 20h10" /><path d="M9 16v4" /><path d="M15 16v4" /></svg>    
                     Hobby
                 </h2>
-              <p class="">As a hobby, I like to build stuff on the web that can solve real problems. Even better if I can try out yet another JS framework!</p>
+              <p>As a hobby, I like to build stuff on the web that can solve real problems. Even better if I can try out yet another JS framework!</p>
             </div>
           </div>
     </div>
--- src/components/Experience.svelte
+++ src/components/Experience.svelte
@@ -1,3 +1,9 @@
+<script>
+  import mcoLogoWhite from "$lib/assets/mco.webp";
+  import mindefLogoWhite from "$lib/assets/mindeflogo.webp";
+  import sngLogoWhite from "$lib/assets/SNGlogo.webp";
+</script>
+
 <h2 id="experience" class="mb-7 text-center flex justify-center"><a href="https://www.linkedin.com/in/zixianchen/">Past work</a>
   <a href="https://www.linkedin.com/in/zixianchen/" class="flex">
     <svg xmlns="http://www.w3.org/2000/svg" class="inline self-center ms-4 hover:fill-green-700" x="0px" y="0px" width="40" height="40" viewBox="0 0 50 50">
@@ -10,13 +16,23 @@
     <div class="timeline-middle">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
     </div>
-    <div class="timeline-start md:text-end mb-10">
-      <time class="font-mono italic">2013</time>
-      <div class="text-lg font-black">Policy Executive @ MINDEF NS Policy</div>
+    <div class="timeline-start md:text-end">
+      <time class="italic text-lg xl:text-xl">2013</time>
+      <div class="md:justify-end">
+        <div class="avatar" style="height:150px">
+          <div class="rounded-full ring ring-gray-200"><img src={mindefLogoWhite} width="150" height="150" alt="NSPD"></div>
+        </div>
+      </div>
+      <div class="text-lg xl:text-3xl font-black">Policy Executive</div>
+      <p class="italic font-bold text-gray-600 mb-3">MINDEF NS Policy</p>
       <p>
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>        Did a bunch of meaningful policy (paper) work</p>
-      <p>
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>        Handled NS policies for sportsmen, citizenship, exit control</p>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>
+        Did a bunch of meaningful policy (paper) work
+      </p>
+      <p class="mb-10">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>
+        Did NS policies for sportsmen, leave, citizenship, exit control
+      </p>
     </div>
     <hr/>
   </li>
@@ -25,13 +41,23 @@
     <div class="timeline-middle">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
     </div>
-    <div class="timeline-end mb-10">
-      <time class="font-mono italic">2015</time>
-      <div class="text-lg font-black">Media Officer @ MINDEF Comms</div>
+    <div class="timeline-end">
+      <time class="italic text-lg xl:text-xl">2015</time>
+      <div class="">
+        <div class="avatar" style="height:150px">
+          <div class="rounded-full ring ring-gray-200"><img src={mcoLogoWhite} width="150" height="150" alt="MCO"></div>
+        </div>
+      </div>
+      <div class="text-lg xl:text-3xl font-black">Communications Officer</div>
+      <p class="italic font-bold text-gray-600 mb-3">@ MINDEF Comms Org</p>
       <p>
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>        Did media/PR work, crisis/reactive comms roles</p>
-      <p>
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>        Ran media events, pitched features, wrote/executed comms plans</p>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>
+        Did media/PR work, crisis/reactive comms roles
+      </p>
+      <p class="mb-10">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>
+        Ran media events, pitched features, wrote/executed comms plans
+      </p>
     </div>
     <hr />
   </li>
@@ -40,13 +66,23 @@
     <div class="timeline-middle">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
     </div>
-    <div class="timeline-start md:text-end mb-10">
-      <time class="font-mono italic">2017</time>
-      <div class="text-lg font-black">Comms Product Owner @ MINDEF Comms</div>
+    <div class="timeline-start md:text-end">
+      <time class="italic text-lg xl:text-xl">2017</time>
+      <div class="md:justify-end">
+        <div class="avatar" style="height:150px">
+          <div class="rounded-full ring ring-gray-200"><img src={mcoLogoWhite} width="150" height="150" alt="MCO"></div>
+        </div>
+      </div>
+      <div class="text-lg xl:text-3xl font-black">Comms Product Owner</div>
+      <p class="italic font-bold text-gray-600 mb-3">@ MINDEF Comms Org</p>
       <p>
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>        Product owner for self built & purchased tooling for media analysts</p>
-      <p>
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>        Raised investments internally and oversaw NLP/CV research projects</p>      
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>
+        Product owner for self built & purchased tooling for media analysts
+      </p>
+      <p class="mb-10">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>
+        Raised investments internally and oversaw NLP/CV research projects
+      </p>      
     </div>
     <hr />
   </li>
@@ -55,13 +91,23 @@
     <div class="timeline-middle">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
     </div>
-    <div class="timeline-end mb-10">
-      <time class="font-mono italic">2021</time>
-      <div class="text-lg font-black">Digital Infrastructure @ Smart Nation SG</div>
+    <div class="timeline-end">
+      <time class="italic text-lg xl:text-xl">2021</time>
+      <div class="">
+        <div class="avatar" style="height:150px">
+          <div class="rounded-full ring ring-gray-200"><img src={sngLogoWhite} width="150" height="150" alt="Smart Nation Group"></div>
+        </div>
+      </div>
+      <div class="text-lg xl:text-3xl font-black">Digital Infrastructure</div>
+      <p class="italic font-bold text-gray-600 mb-3">@ Smart Nation SG</p>
       <p>
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>        Paper pusher for Govt on commercial cloud, Govt data centre, SG Govt tech stack, employee tooling</p>
-      <p>
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>        Assessed funding requests from Govt agencies trying to start digitalization & modernization projects</p>          
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>
+        Paper pusher for Govt on commercial cloud, Govt data centre, SG Govt tech stack, employee tooling
+      </p>
+      <p class="mb-10">
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>
+        Assessed funding requests from Govt agencies trying to start digitalization & modernization projects
+      </p>          
     </div>
     <hr />
   </li>
@@ -71,15 +117,25 @@
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="green" class="h-5 w-5 fill-lime-600"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd" /></svg>
     </div>
     <div class="timeline-start md:text-end mb-10">
-      <time class="font-mono italic">2023</time>
-      <div class="text-lg font-black">Product Owner @ MINDEF Service Delivery</div>
+      <time class="italic text-lg xl:text-xl">2023</time>
+      <div class="md:justify-end">
+        <div class="avatar" style="height:150px">
+          <div class="rounded-full ring ring-gray-200"><img src={mindefLogoWhite} width="150" height="150" alt="Service Delivery Division"></div>
+        </div>
+      </div>
+      <div class="text-lg xl:text-3xl font-black">Product Owner</div>
+      <p class="italic font-bold text-gray-600 mb-3">@ MINDEF Service Delivery</p>
       <p>
         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>
         Owns internal employee tools/systems for employee services</p>
       <p>
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>        Manage outsourced engineering/dev teams</p>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>
+        Manage outsourced engineering/dev teams
+      </p>
       <p>
-        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>        Paper pusher for opportunities to use technology for service delivery</p>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-plus inline" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" fill="black" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" /><path d="M9 12h6" stroke="white" /><path d="M12 9v6" stroke="white" /></svg>
+        Paper pusher for opportunities to use technology for service delivery
+      </p>
     </div>
     <hr style="background-color: #65a30d" />
   </li>
