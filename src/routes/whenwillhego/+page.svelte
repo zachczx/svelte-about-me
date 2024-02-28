b509a6946caf72d8aaaa12cdeb5624b8282dfcc5
zachczx <zixian.chen@gmail.com>
2024-02-28 23:43:28 +0800

    -

--- src/routes/whenwillhego/+page.svelte
+++ src/routes/whenwillhego/+page.svelte
@@ -15,26 +15,56 @@
             clearInterval(x)
         }
     }, 1000)
+    let random = Math.floor(Math.random() * 100)
+    let text = 1
+    if (random <= 33) {
+      text = 1
+    } else if (random <= 66 && random > 33) {
+      text = 2 
+    } else {
+      text = 3
+    }
 </script>
     
     <svelte:head>
     <title>Countdown time</title>
 </svelte:head>
-<section id="Main" class="bg-greengreen h-dvh p-5">
-<h1 class="bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-black mb-5 pb-3 text-center">When will he go?</h1>
+<section id="Main" class="bg-image h-dvh p-5">
+<h1 class="bg-clip-text text-transparent bg-gradient-to-r from-orange-200 to-white mb-5 pb-3 text-center">When will he go?</h1>
 
 <div class="grid justify-items-center">
-    <div class="text-xl italic font-semibold mt-3">
-        <p class="mb-5">
+    <div class="text-xl italic mt-3 rounded-lg bg-red-200 p-5 mb-10">
+        <p class="">
             <svg xmlns="http://www.w3.org/2000/svg" class="inline me-2" width="24" height="24" viewBox="0 0 24 24"><path d="M13 14.725c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275zm-13 0c0-5.141 3.892-10.519 10-11.725l.984 2.126c-2.215.835-4.163 3.742-4.38 5.746 2.491.392 4.396 2.547 4.396 5.149 0 3.182-2.584 4.979-5.199 4.979-3.015 0-5.801-2.305-5.801-6.275z"/></svg>            
-            Cold coffee chills dreams,<br>
-            Papers whisper of forced goodbyes,<br>
-            A hollow laugh escapes.
-        </p>
-        <p class="mb-10">
-            But then, a crack in the facade,<br>
-            Sunlight spills on a dusty path,<br>
-            Hope unfurls its wings.<svg xmlns="http://www.w3.org/2000/svg" class="inline ms-2" width="24" height="24" viewBox="0 0 24 24"><path d="M11 9.275c0 5.141-3.892 10.519-10 11.725l-.984-2.126c2.215-.835 4.163-3.742 4.38-5.746-2.491-.392-4.396-2.547-4.396-5.149 0-3.182 2.584-4.979 5.199-4.979 3.015 0 5.801 2.305 5.801 6.275zm13 0c0 5.141-3.892 10.519-10 11.725l-.984-2.126c2.215-.835 4.163-3.742 4.38-5.746-2.491-.392-4.396-2.547-4.396-5.149 0-3.182 2.584-4.979 5.199-4.979 3.015 0 5.801 2.305 5.801 6.275z"/></svg>
+            {#if text === 1}
+            Armed with knowledge, a digital knight,<br>
+            To slay the forms and make systems alight.<br>
+            But met with relics, dusty and slow,<br>
+            And colleagues who thought "cloud" meant a place for the snow.<br><br>
+            My Kanban dreams met paper chase woes,<br>
+            Progress a whisper, lost in bureaucratic flows.<br>
+            So long, dear government, progress unknown,<br>
+            My expertise seeks a fertile field, a purpose to be sown.
+            {:else if text === 2}
+            Joined the gov't, bright-eyed and keen,<br>
+            To streamline forms, a coding machine.<br>
+            Met endless approvers with an approval queue,<br>
+            And colleagues who thought "cloud" was a shade of blue.<br><br>
+            My agile sprints met fax machine hums,<br>
+            Ideas drowned in process's mumbling chums.<br>
+            So long, bureaucracy, where progress sleeps,<br>
+            My skills are better suited elsewhere, it seems.
+            {:else}
+            Came to serve, code in hand,<br>
+            To slay the forms, make the nation grand.<br>
+            But met with systems, ancient and slow,<br>
+            And colleagues who thought "API" meant "hello."<br><br>
+            My Kanban dreams met paper chase woes,<br>
+            Innovation lost, buried in red tape's nose.<br>
+            So long, dear government, progress unknown,<br>
+            My talents seek a fertile codebase, a digital throne.
+            {/if}
+          <svg xmlns="http://www.w3.org/2000/svg" class="inline ms-2" width="24" height="24" viewBox="0 0 24 24"><path d="M11 9.275c0 5.141-3.892 10.519-10 11.725l-.984-2.126c2.215-.835 4.163-3.742 4.38-5.746-2.491-.392-4.396-2.547-4.396-5.149 0-3.182 2.584-4.979 5.199-4.979 3.015 0 5.801 2.305 5.801 6.275zm13 0c0 5.141-3.892 10.519-10 11.725l-.984-2.126c2.215-.835 4.163-3.742 4.38-5.746-2.491-.392-4.396-2.547-4.396-5.149 0-3.182 2.584-4.979 5.199-4.979 3.015 0 5.801 2.305 5.801 6.275z"/></svg>
         </p>
     </div>
 </div>
@@ -66,7 +96,7 @@
     </div>
 </div>
 
-<div class="text-center mt-10"><button class="btn btn-accent hover:bg-red-600 text-2xl" on:click={() => {trigger = !trigger}}><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-confetti me-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 5h2" /><path d="M5 4v2" /><path d="M11.5 4l-.5 2" /><path d="M18 5h2" /><path d="M19 4v2" /><path d="M15 9l-1 1" /><path d="M18 13l2 -.5" /><path d="M18 19h2" /><path d="M19 18v2" /><path d="M14 16.518l-6.518 -6.518l-4.39 9.58a1 1 0 0 0 1.329 1.329l9.579 -4.39z" /></svg>Celebrate prematurely</button></div>
+<div class="text-center mt-10"><button class="btn bg-red-600 text-white hover:bg-pink-500 text-2xl" on:click={() => {trigger = !trigger}}><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-confetti me-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 5h2" /><path d="M5 4v2" /><path d="M11.5 4l-.5 2" /><path d="M18 5h2" /><path d="M19 4v2" /><path d="M15 9l-1 1" /><path d="M18 13l2 -.5" /><path d="M18 19h2" /><path d="M19 18v2" /><path d="M14 16.518l-6.518 -6.518l-4.39 9.58a1 1 0 0 0 1.329 1.329l9.579 -4.39z" /></svg>Celebrate prematurely</button></div>
 {#if difference < 0 || trigger === true}
 <div class="pyro">
     <div class="before"></div>
@@ -77,12 +107,12 @@
 
     <style>
 :root {
-    overflow-y: clip;
+    overflow-y: none;
 }
 
-.bg-greengreen {
+.bg-image {
     background-color: #FFCC00;
-    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25' viewBox='0 0 1600 800'%3E%3Cg %3E%3Cpath fill='%23fcd460' d='M486 705.8c-109.3-21.8-223.4-32.2-335.3-19.4C99.5 692.1 49 703 0 719.8V800h843.8c-115.9-33.2-230.8-68.1-347.6-92.2C492.8 707.1 489.4 706.5 486 705.8z'/%3E%3Cpath fill='%23f8dc87' d='M1600 0H0v719.8c49-16.8 99.5-27.8 150.7-33.5c111.9-12.7 226-2.4 335.3 19.4c3.4 0.7 6.8 1.4 10.2 2c116.8 24 231.7 59 347.6 92.2H1600V0z'/%3E%3Cpath fill='%23f4e3a6' d='M478.4 581c3.2 0.8 6.4 1.7 9.5 2.5c196.2 52.5 388.7 133.5 593.5 176.6c174.2 36.6 349.5 29.2 518.6-10.2V0H0v574.9c52.3-17.6 106.5-27.7 161.1-30.9C268.4 537.4 375.7 554.2 478.4 581z'/%3E%3Cpath fill='%23f1eabf' d='M0 0v429.4c55.6-18.4 113.5-27.3 171.4-27.7c102.8-0.8 203.2 22.7 299.3 54.5c3 1 5.9 2 8.9 3c183.6 62 365.7 146.1 562.4 192.1c186.7 43.7 376.3 34.4 557.9-12.6V0H0z'/%3E%3Cpath fill='%23EDF1D6' d='M181.8 259.4c98.2 6 191.9 35.2 281.3 72.1c2.8 1.1 5.5 2.3 8.3 3.4c171 71.6 342.7 158.5 531.3 207.7c198.8 51.8 403.4 40.8 597.3-14.8V0H0v283.2C59 263.6 120.6 255.7 181.8 259.4z'/%3E%3Cpath fill='%23dfe8c9' d='M1600 0H0v136.3c62.3-20.9 127.7-27.5 192.2-19.2c93.6 12.1 180.5 47.7 263.3 89.6c2.6 1.3 5.1 2.6 7.7 3.9c158.4 81.1 319.7 170.9 500.3 223.2c210.5 61 430.8 49 636.6-16.6V0z'/%3E%3Cpath fill='%23d1dfbc' d='M454.9 86.3C600.7 177 751.6 269.3 924.1 325c208.6 67.4 431.3 60.8 637.9-5.3c12.8-4.1 25.4-8.4 38.1-12.9V0H288.1c56 21.3 108.7 50.6 159.7 82C450.2 83.4 452.5 84.9 454.9 86.3z'/%3E%3Cpath fill='%23c1d5ad' d='M1600 0H498c118.1 85.8 243.5 164.5 386.8 216.2c191.8 69.2 400 74.7 595 21.1c40.8-11.2 81.1-25.2 120.3-41.7V0z'/%3E%3Cpath fill='%23b0cb9d' d='M1397.5 154.8c47.2-10.6 93.6-25.3 138.6-43.8c21.7-8.9 43-18.8 63.9-29.5V0H643.4c62.9 41.7 129.7 78.2 202.1 107.4C1020.4 178.1 1214.2 196.1 1397.5 154.8z'/%3E%3Cpath fill='%239DC08B' d='M1315.3 72.4c75.3-12.6 148.9-37.1 216.8-72.4h-723C966.8 71 1144.7 101 1315.3 72.4z'/%3E%3C/g%3E%3C/svg%3E");
+    background-image: url("../../lib/assets/amin-moshrefi-ebHg5_4CHL0-unsplash.webp");
     background-attachment: fixed;
     background-size: cover;
 }
